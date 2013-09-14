23ccfc006f95529b7d8a9fe1cfdf5d2d7c3885cc
helloabob <wangbo@wangbos-Mac-Pro.local>
2013-09-15 06:26:29 +0800

    Initial Commit

---
+++ SevenKingFiveTwoThreee/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SevenKingFiveTwoThreee
+//
+//  Created by wangbo on 9/15/13.
+//  Copyright (c) 2013 wangbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SevenKingFiveTwoThreee/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SevenKingFiveTwoThreee
+//
+//  Created by wangbo on 9/15/13.
+//  Copyright (c) 2013 wangbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
